be6c35193011c2ca4e5e4483b6692b93cc802664
Ronaldo Correa <34607760+ronaldohack@users.noreply.github.com>
2020-09-11 11:57:46 -0300

    Update testes.py

--- src/testes.py
+++ src/testes.py
@@ -14,7 +14,7 @@ else:
 			erroInicializar = True
 		else:
 			for elemento in linha:
-				if elemento != 'x':
+				if elemento != '.':
 					erroInicializar = True
 if erroInicializar:
 	sys.exit(1)
